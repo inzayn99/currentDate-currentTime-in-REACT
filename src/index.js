c31fa2fe67eea2767c70fa1a13b142ab82faee48
Firoj <mymindhungry@gmail.com>
2021-09-13 16:16:26 +0545

    CurrDate & CurrTime dynamically

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "KHAN";
+const currDate = new Date().toLocaleDateString();
+const currTime = new Date().toLocaleTimeString();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <>
+  <h1>Hello, my name is {name}</h1>
+  <p>current date  is = {currDate}</p>
+  <p>current time  is = {currTime}</p>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </>,
+  document.getElementById('root'));
